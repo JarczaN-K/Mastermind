6a20fabd137124fad5dd4e967ad053b718fc05c1
JarczaN-K <Lothar55D120>
2020-12-19 21:39:38 +0100

    first classes

--- src/Machine/Machine.java
+++ src/Machine/Machine.java
@@ -1,9 +1,26 @@
 package Machine;
 
+import java.util.Arrays;
+
 public class Machine {
-    private String[] cipher;
+    private String[] cipher = new String[]{"W", "G"};
+    String[] colours = new String[] {"W", "G", "O", "R", "B", "V"};
+
+    private int randomize(String[] randomizer){
+        int length = randomizer.length;
+        return 0;
+    }
+
+    public void start(){
+        System.out.println(Arrays.toString(cipher));
+    }
+
 
     public String[] createCipher(){
-        return null;
+        return cipher;
+    }
+
+    public int getCipherLength() {
+        return cipher.length;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import Machine.Machine;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Machine start = new Machine();
+        start.start();
+
 
     }
 }
